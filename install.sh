ad30e8594270ffc79b4b27a41f01dbeb3013d09d
aabaza <amin.abaza55@gmail.com>
2022-09-05 14:38:53 +0200

    add volume

--- install.sh
+++ install.sh
@@ -39,30 +39,30 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
-# #Install EksCtl
-# echo "--------------------Installing EKS-CTL--------------------"
-# sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-# sudo mv /tmp/eksctl /usr/local/bin
-# #Install Kubectl
-# echo "----------"----------Installing Kubectl"--------------------"
-# sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-# echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
-# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-# #Install Minikube
-# echo "--------------------Installing Minikube--------------------"
-# sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-# sudo install minikube-linux-amd64 /usr/local/bin/minikube
-# #Install K9s
-# echo "--------------------Installing K9s--------------------"
-# wget https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz
-# sudo tar -xvzf k9s_Linux_x86_64.tar.gz
-# sudo install -m 755 k9s /usr/local/bin
-# #Install AWS-CLI
-# echo "--------------------Installing AWS-CLI--------------------"
-# sudo apt-get install zip -y
-# sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
-# sudo ./aws/install
+#Install EksCtl
+echo "--------------------Installing EKS-CTL--------------------"
+sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+#Install Kubectl
+echo "----------"----------Installing Kubectl"--------------------"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#Install Minikube
+echo "--------------------Installing Minikube--------------------"
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+#Install K9s
+echo "--------------------Installing K9s--------------------"
+wget https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz
+sudo tar -xvzf k9s_Linux_x86_64.tar.gz
+sudo install -m 755 k9s /usr/local/bin
+#Install AWS-CLI
+echo "--------------------Installing AWS-CLI--------------------"
+sudo apt-get install zip -y
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
+sudo ./aws/install
 #Add docker to sudo group
 echo "--------------------Add Docker to Sudo group--------------------"
 sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker && sudo chmod 777 /var/run/docker.sock
