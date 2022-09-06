26dc4f7573bdbf91f2f76e068f16bd045f2966c8
aabaza <amin.abaza55@gmail.com>
2022-09-06 11:37:58 +0200

    testwebhook

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,11 +1,11 @@
 resource "aws_security_group" "allow_ports" {
-    ingress {
-        description = "allow ssh ports"
-        from_port        = 22
-        to_port          = 22
-        protocol          = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
+    # ingress {
+    #     description = "allow ssh ports"
+    #     from_port        = 22
+    #     to_port          = 22
+    #     protocol          = "tcp"
+    #     cidr_blocks      = ["0.0.0.0/0"]
+    # }
     ingress {
         description = "allow port for jenkins"
         from_port = 8080
